6a84976a030adcb0fc9fbc937428831130143876
yeetersplebears <janamouroux@gmail.com>
2025-04-24 23:21:51 +0000

    UPDADE_file_clusteror_predict_Npray

--- data.preparation/PreparatorQuantitativ.R
+++ data.preparation/PreparatorQuantitativ.R
@@ -8,7 +8,7 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 # open datasets : 
 ################################################################################
 
-CatAus = read.csv("../data/Pet Cats Australia.csv"
+CatAus = read.csv("../data/PetCatsAustralia.csv"
                   , stringsAsFactors = F)
 CatAusGPS = read.csv("../data/PetCatsAustraliaGPS"
                      , stringsAsFactors = F)
@@ -56,7 +56,7 @@ date_pattern = "(\\d{4}-\\d{2}-\\d{2})"
 time_pattern = "(\\d{2}:\\d{2}:\\d{2}\\.\\d{3})"
 
 # the patern to transform "6 years" into 6 : 
-year.pattern = "{\\d|\\.}+(?=\\s*years)"
+year.pattern = "\\d+(\\.\\d+)?(?=\\s*(years?))"
 
 ## 3. using a for loop to apply each pattern in order to extract the data
 
